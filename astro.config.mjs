cbf1538d58fd614b06b5db74f804a47c05c95e1a
Johan Lindgren <Johan.Lindgren@xlent.se>
2024-10-28 10:52:43 +0100

    docs: basic doc structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,58 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Simplified .NET",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Getting started",
+          items: [
+            { label: "Introduction", link: "/getting-started/introduction" },
+            {
+              label: "Getting started",
+              link: "/getting-started/overview",
+            },
+          ],
+        },
+        {
+          label: "Core Concepts",
+          autogenerate: { directory: "core-concepts" },
+        },
+        {
+          label: "Building Web Apps",
+          autogenerate: { directory: "building-web-apps" },
+        },
+        {
+          label: "Building APIs",
+          autogenerate: { directory: "building-apis" },
+        },
+        {
+          label: "Entity Framework Core",
+          autogenerate: { directory: "entity-framework-core" },
+        },
+        {
+          label: "Blazor",
+          autogenerate: { directory: "blazor" },
+        },
+        {
+          label: "Advanced Topics",
+          autogenerate: { directory: "advanved-topics" },
+        },
+        {
+          label: "Docker",
+          autogenerate: { directory: "docker" },
+        },
+        {
+          label: "Contribute",
+          link: "contribute",
+        },
+      ],
+    }),
+  ],
 });
